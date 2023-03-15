6f46216a5b0ada1f3f7542703d356a5bc72c3a53
abhishek4nz <abhishek4nz@gmail.com>
2023-03-15 18:35:27 +0530

    Extended BaseClass with page object

--- src/test/java/com/hybrid/pageObjects/LoginPage.java
+++ src/test/java/com/hybrid/pageObjects/LoginPage.java
@@ -1,35 +1,31 @@
 package com.hybrid.pageObjects;
 
+import com.hybrid.testCases.BaseClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends BaseClass {
 
-    WebDriver ldriver;          //CREATING A LOCAL WEBDROVER OBJECT
-
+//    WebDriver ldriver;          //CREATING A LOCAL WEBDROVER OBJECT
     //CREATE A CONSTRUCTOR TO BE CALLED FROM TEST CASE CLASS
-    public LoginPage(WebDriver rdriver) {
+//    DRIVER IS INHERITED FROM THE BASE CLASS
+    public LoginPage(WebDriver driver) {
 
-        ldriver = rdriver;                //ASSIGNING REMOTE WEBDRIVER OBJECT TO LOCAL WEBDRIVER
-        PageFactory.initElements(rdriver, this);
+//        this.driver = driver;                //ASSIGNING REMOTE WEBDRIVER OBJECT TO LOCAL WEBDRIVER
+        PageFactory.initElements(driver, this);
     }
 
     //FIRST WE HAVE TO LOCATE THE ELEMENTS IN THE PAGE AND STORE THEM IN MEMEORY FOR LATER USAGE
-
-    @FindBy(name = "uid")
-    @CacheLookup           //USING LOCATIORS TO LOCATE ELEMENTS IN THE LOGIN PAGE
-
+    @FindBy(name = "uid")    @CacheLookup           //USING LOCATIORS TO LOCATE ELEMENTS IN THE LOGIN PAGE
     WebElement txtUserName;         //STORING THE USERNAME FIELD AS A WEBELEMENT
 
-    @FindBy(name = "password")
-    @CacheLookup
+    @FindBy(name = "password")    @CacheLookup
     WebElement txtPassword;
 
-    @FindBy(name = "btnLogin")
-    @CacheLookup
+    @FindBy(name = "btnLogin")    @CacheLookup
     WebElement btnLogin;
 
     //SECONDLY CREATE ACTION METHODS TO PERFORM ACTION ON THE WEB ELEMENTS
